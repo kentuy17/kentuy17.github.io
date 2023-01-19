60f9d86e45744a45a22a53fae5cfde1b4a76925e
kennethcamacho <kentuy17@gmail.com>
2023-01-19 19:58:54 +0800

    Initial

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+
+// portfolio
+import NavbarComponent from './components/portfolio/NavbarComponent.vue'
+import HomeComponent from './components/portfolio/sections/HomeComponent.vue'
+import AboutComponent from './components/portfolio/sections/AboutComponent.vue'
+import ResumeComponent from './components/portfolio/sections/ResumeComponent.vue'
+import ServicesComponent from './components/portfolio/sections/ServicesComponent.vue'
+import SkillsComponent from './components/portfolio/sections/SkillsComponent.vue'
+import ProjectsComponent from './components/portfolio/sections/ProjectsComponent.vue'
+import BlogComponent from './components/portfolio/sections/BlogComponent.vue'
+import CounterComponent from './components/portfolio/sections/CounterComponent.vue'
+import HireMeComponent from './components/portfolio/sections/HireMeComponent.vue'
+import ContactComponent from './components/portfolio/sections/ContactComponent.vue'
+
+</script>
+
+<template> <!-- body -->
+  <NavbarComponent />
+  <HomeComponent />
+  <AboutComponent />
+  <ResumeComponent />
+  <ServicesComponent />
+  <SkillsComponent />
+  <ProjectsComponent />
+  <BlogComponent />
+  <ContactComponent />
+  <HireMeComponent  />
+  <ContactComponent />
+</template>
+
+
